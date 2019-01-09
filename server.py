a532c8bdb8d9e3d99ebafb403f02695bc25393dc
Lawrence Neal <nealla@lwneal.com>
2019-01-09 14:40:35 -0800

    Start with simple workflow from intro page

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 import flask
+import datetime
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def hello():
-    return flask.render_template('index.html')
+    kwargs = {
+        'current_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    }
+    return flask.render_template('index.html', **kwargs)
+
+
+@app.route('/submit_page1', methods=['POST'])
+def submit_page1():
+    # TODO: save the user's input to a file
+    print('Submission from page 1: {}'.format(flask.request.data))
+    return flask.redirect('static/page2.html')
+
 
 @app.route('/static/<path:path>')
 def serve_static(path):
