a792017e8ad4acb3d47d0c31ff2183bc6e8a14ef
Lawrence Neal <nealla@lwneal.com>
2019-02-07 10:31:42 -0800

    Move 'user_id' to leftmost column of output CSV

--- parse_user_data.py
+++ parse_user_data.py
@@ -5,9 +5,11 @@ import pandas as pd
 INPUT_DATA_DIR = 'user_data/'
 OUTPUT_FILENAME = 'collected_data.csv'
 
-# Collect all endpoints per user_id
+# Collect all the .json data files written by server.py:save_request
 entries = {}
 print('Reading user data from {}'.format(INPUT_DATA_DIR))
+
+# Load each .json file and add it to entries, sorted by user_id
 for filename in sorted(os.listdir(INPUT_DATA_DIR)):
     if not filename.endswith('.json'):
         continue
@@ -18,8 +20,13 @@ for filename in sorted(os.listdir(INPUT_DATA_DIR)):
         entries[user_id] = {}
     entries[user_id].update(data)
 
-
-# Convert to a CSV file
-print('Writing user data to CSV file: {}'.format(OUTPUT_FILENAME))
+# Convert the dictionary to a Pandas dataframe
 data_rows = [i for i in entries.values()]
-pd.DataFrame(data_rows).to_csv(OUTPUT_FILENAME)
+df = pd.DataFrame(data_rows)
+
+# Put user_id into the leftmost column so it looks nice
+df = df[['user_id'] + df.columns.tolist()]
+
+# Write the dataframe as a CSV file
+print('Writing user data to CSV file: {}'.format(OUTPUT_FILENAME))
+df.to_csv(OUTPUT_FILENAME)
